9a5960f14f769700718d3daa0756bbea49bfe61d
David Schenkel <david.schenkel@heig-vd.ch>
2022-05-31 15:49:07 +0200

    controllers ressources made

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HoraireController extends Controller
+class ResultController extends Controller
 {
     /**
      * Display a listing of the resource.
--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TacheController extends Controller
+class ReviewController extends Controller
 {
     /**
      * Display a listing of the resource.
