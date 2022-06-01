e12705cc911b10b82956e099083edad110f74b78
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2022-06-01 11:35:31 +0200

    Task controller and task request are done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ScheduleController;
 
 /*
@@ -30,3 +31,5 @@ Route::get('/horaire', [ScheduleController::class, 'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/tasks', [TaskController::class, 'index']);
