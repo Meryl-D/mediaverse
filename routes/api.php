6401be638b042152671c763df2cfd57352b5ce16
Meryl Dubois <meryl.dubois@heig-vd.ch>
2022-06-02 13:29:34 +0200

    config api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ResultController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\AbsenceController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\NotificationController;
 
 /*
 |--------------------------------------------------------------------------
