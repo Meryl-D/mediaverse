785b1657d0c3e22070cd8e86fc44326f057558bd
David Schenkel <david.schenkel@heig-vd.ch>
2022-06-03 15:01:53 +0200

    review component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,85 +1,90 @@
 <script setup>
-    import { ref } from "vue";
-    import axios from "axios";
-    import TheSchedule from './components/TheSchedule.vue'
-    import TheResults from './components/TheResults.vue'
-    import TheAbsences from './components/TheAbsences.vue'
-    import TheAccount from './components/TheAccount.vue'
-    import TheLogin from './components/TheLogin.vue'
+import { ref } from "vue";
+import axios from "axios";
+import TheSchedule from './components/TheSchedule.vue'
+import TheResults from './components/TheResults.vue'
+import TheAbsences from './components/TheAbsences.vue'
+import TheAccount from './components/TheAccount.vue'
+import TheLogin from './components/TheLogin.vue'
+import TheReviews from './components/TheReviews.vue'
 
-    // const message = ref('hello from vue');
-    // const message2 = ref(data);
+// const message = ref('hello from vue');
+// const message2 = ref(data);
 
-    // fetch('/api/test').then(
-    // 	resp => resp.json()
-    // ).then(
-    // 	test => console.log(test)
-    // )
+// fetch('/api/test').then(
+// 	resp => resp.json()
+// ).then(
+// 	test => console.log(test)
+// )
 
-    const isLoggedIn = ref("false");
+const isLoggedIn = ref("false");
 
-    const routes = {
-        '#': {
+const routes = {
+    '#': {
         label: 'Schedule',
         component: TheSchedule,
-        },
-        '#notes': {
+    },
+    '#notes': {
         label: 'Results',
         component: TheResults,
-        },
-        '#absences': {
+    },
+    '#absences': {
         label: 'Absences',
         component: TheAbsences,
-        },
-        '#compte': {
+    },
+    '#compte': {
         label: 'Account',
         component: TheAccount,
-        },
-    };
+    },
+    '#review': {
+        label: 'Review',
+        component: TheReviews,
+    },
+};
 
-    if (window.Laravel.isLoggedin) {
+if (window.Laravel.isLoggedin) {
     this.isLoggedIn = true;
-    }
+}
 
-    /**
-     * Logs out the user.
-     * 
-     * @param mixed evt
-     * 
-     * @return void
-     */
-    function logout(evt) {
-        console.log("logout");
-        evt.preventDefault();
+/**
+ * Logs out the user.
+ * 
+ * @param mixed evt
+ * 
+ * @return void
+ */
+function logout(evt) {
+    console.log("logout");
+    evt.preventDefault();
 
-        axios.get("/sanctum/csrf-cookie").then((response) => {
+    axios.get("/sanctum/csrf-cookie").then((response) => {
 
-            axios.post("/api/logout").then((response) => {
+        axios.post("/api/logout").then((response) => {
 
-                if (response.data.success) {
+            if (response.data.success) {
                 window.location.href = "/";
-                } else {
+            } else {
                 console.log(response);
-                }
-            })
+            }
+        })
             .catch(function (error) {
                 console.error(error);
             });
-        });
-    }
+    });
+}
 
-    const hash = ref(window.location.hash);
-    window.addEventListener('hashchange', () => hash.value = window.location.hash);
-    const curHash = computed(() => routes[hash.value] ? hash.value : Object.keys(routes)[0]);
-    const curComponent = computed(() => routes[curHash.value].component);
+const hash = ref(window.location.hash);
+window.addEventListener('hashchange', () => hash.value = window.location.hash);
+const curHash = computed(() => routes[hash.value] ? hash.value : Object.keys(routes)[0]);
+const curComponent = computed(() => routes[curHash.value].component);
 </script>
 
 
 <template>
-  <!-- <h1>
+    <!-- <h1>
         {{ message }}
     </h1> -->
-  <!-- <h2>
+    <!-- <h2>
          {{message2}}
     </h2> -->
     <!-- for logged-in user-->
@@ -89,30 +94,30 @@
         <main>
             <template v-for="(route, hash) of routes" :key="route">
                 <div v-show="hash == curHash">
-                    <component :is="curComponent"/>
+                    <component :is="curComponent" />
                 </div>
             </template>
         </main>
     </div>
     <!-- for non-logged user-->
     <div v-else>
-      <the-login></the-login>
+        <the-login></the-login>
     </div>
 </template>
 
 <style scoped>
-    * {
-        /**
+* {
+    /**
         * Colors
         */
-        --orange: #F9A228;
-        --green: #2A3A30;
-        --beige: #F1DFCD;
-        --white: #FFFCFA;
+    --orange: #F9A228;
+    --green: #2A3A30;
+    --beige: #F1DFCD;
+    --white: #FFFCFA;
 
-        /**
+    /**
         * 
         */
 
-    }
+}
 </style>
