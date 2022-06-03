10623545e1db1afe6ad6707d57c653677e27e964
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2022-06-03 15:49:32 +0200

    base button component

---
+++ resources/js/components/subComponents/BaseButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: "validate",
+    required: true,
+  },
+});
+console.log(props.type);
+</script>
+
+<template>
+  <button >
+      <!-- insérer le contenu du parent -->
+      <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  color:#F9A228;
+  background-color: #FFFCFA;
+  border-color: #F9A228;
+  border: 1em;
+  border-radius: 25rem;
+}
+</style>
