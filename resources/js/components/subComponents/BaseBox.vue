3713cdcb9892cee64dd14178ced5985d5bc5edb9
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2022-06-07 12:01:19 +0200

    created base box

---
+++ resources/js/components/subComponents/BaseBox.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <!-- insérer le contenu du parent -->
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  background-color: var(--white);
+  border: none;
+  border-radius: 0.3em;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+}
+</style>
