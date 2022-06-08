9565d8ef5785c8c3be14df77c248dbc102dd200b
Heloise Cuche <heloise.cuche@heig-vd.ch>
2022-06-08 15:24:42 +0200

    modify result controller

--- resources/js/components/TheResults.vue
+++ resources/js/components/TheResults.vue
@@ -1,48 +1,48 @@
 <script async setup>
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
 import { axiosClient } from "../utils/axios.js";
-
+import BaseButton from "../components/subComponents/BaseButton.vue";
 
 async function results() {
-  const { data } = await axiosClient.get('api/results');
-  return data
+  const { data } = await axiosClient.get("api/results");
+  return data;
 }
 
-const allData = await results()
-const module = allData.modules
-console.log(allData)
+const allData = await results();
+console.log(allData);
 
-const test = Object.keys(allData).length
-console.log(test)
-
-let tabResults = []
-let compteur = 0
+//get the semester
+let maxSemester = 0
 for (const result in allData) {
     if (Object.hasOwnProperty.call(allData, result)) {
-        let oneResult = allData[result][compteur]
-        console.log("c'est un ", result, allData[result][compteur])
+        const element = allData[result];
+        if(element.semesterNo >= compteur){
+            compteur = element.semesterNo
+        }
     }
-}
-
+} 
 
 </script>
 
 <template>
-<h1>Notes</h1>
+  <h1>Notes</h1>
 
-        <ul>
-            <li v-for="result in module" :key="result.id">
-                <div>{{ result.name }}</div>
-            </li>
-        </ul>
-        <h2>Cours</h2>
-          <ul>
-            <li v-for="result in allData.results" :key="result.id">
-                <div>{{ result.grade }}</div>
-            </li>
-        </ul>
+  <!-- <div>
+    <span v-for="n in 10">{{ n }} </span>
+  </div> -->
 
+  <base-button type="submit">Envoyer</base-button>
+
+  <ul>
+    <li v-for="result in allData" :key="result.grade">
+      <div>{{ result.CourseName }}</div>
+      <div>{{ result.grade }}</div>
+
+      <br />
+    </li>
+  </ul>
 </template>
 
 
-<style></style>
+<style scoped>
+</style>
