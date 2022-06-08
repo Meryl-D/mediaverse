25df38c68756c54cfb663b3961ddcd795bcf19a2
Heloise Cuche <heloise.cuche@heig-vd.ch>
2022-06-08 16:10:52 +0200

    result

--- resources/js/components/TheResults.vue
+++ resources/js/components/TheResults.vue
@@ -2,6 +2,7 @@
 import { ref, computed } from "vue";
 import { axiosClient } from "../utils/axios.js";
 import BaseButton from "../components/subComponents/BaseButton.vue";
+import BaseBox from "../components/subComponents/BaseBox.vue";
 
 async function results() {
   const { data } = await axiosClient.get("api/results");
@@ -11,38 +12,47 @@ async function results() {
 const allData = await results();
 console.log(allData);
 
-//get the semester
-let maxSemester = 0
+//get semester
+let maxSemester = 0;
 for (const result in allData) {
-    if (Object.hasOwnProperty.call(allData, result)) {
-        const element = allData[result];
-        if(element.semesterNo >= compteur){
-            compteur = element.semesterNo
-        }
+  if (Object.hasOwnProperty.call(allData, result)) {
+    const element = allData[result];
+    if (element.semesterNo >= maxSemester) {
+      maxSemester = element.semesterNo;
     }
-} 
+  }
+}
 
+const semester = ref(0);
+
+function smsChoosen(id) {
+  semester.value = id
+  console.log(semester.value);
+}
+
+console.log(allData);
 </script>
 
 <template>
   <h1>Notes</h1>
 
-  <!-- <div>
-    <span v-for="n in 10">{{ n }} </span>
-  </div> -->
+  <div>
+    <span v-for="i in maxSemester" :key="i">
+      <base-button @click="smsChoosen(i)" type="submit">S{{ i }}</base-button>
+    </span>
+  </div>
 
-  <base-button type="submit">Envoyer</base-button>
-
-  <ul>
-    <li v-for="result in allData" :key="result.grade">
-      <div>{{ result.CourseName }}</div>
-      <div>{{ result.grade }}</div>
-
-      <br />
-    </li>
-  </ul>
+  <span v-for="result in allData" :key="result.grade">
+    <base-box>
+      <div>{{ result.moduleName }} |</div>
+      <div>
+        | <strong>{{ result.CourseName }}</strong> |
+      </div>
+      <div>| {{ result.gradeWeight }} |</div>
+      <div>| {{ result.grade }}</div>
+    </base-box>
+  </span>
 </template>
-
 
 <style scoped>
 </style>
