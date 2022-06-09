adff72c0d1c6586dc219a62d904787e8a08eab88
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2022-06-09 09:58:24 +0200

    added some css

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -56,8 +56,20 @@ async function logout() {
     url(../fonts/SuisseScreen-Medium.otf) format("otf"),
     url(../fonts/SuisseScreen-Bold.otf) format("otf");
 }
+.h1{
+  font-size: 2rem;
+}
+.h2{
+  font-size: 1.3rem;
+}
+.bignote{
+  font-size: 1.5rem;
+}
 .p {
   font-size: 1.1rem;
+}
+.psmall {
+  font-size: 1rem;
 }
 .bold {
   font-weight: bold;
