fe631cdbe0f2c37517c63c18abeee387897ab4f3
David Schenkel <david.schenkel@heig-vd.ch>
2022-06-10 11:08:50 +0200

    tasks component

--- resources/js/components/subComponents/TheTasks.vue
+++ resources/js/components/subComponents/TheTasks.vue
@@ -12,127 +12,55 @@ async function tasks() {
 const allDatas = await tasks();
 console.log(allDatas);
 
-// let dataFiltred = []
-// let n = 0
-// allData.forEach(d => {
-//   if (d.resultId == allData[0].resultId) {
-//     dataFiltred[n] = d
-//     n++
-//   }
-// });
-// console.log(dataFiltred)
-
-
-
-// //get semester
-// let maxSemester = 0;
-// for (const result in allData) {
-//   if (Object.hasOwnProperty.call(allData, result)) {
-//     const element = allData[result];
-//     if (element.semesterNo >= maxSemester) {
-//       maxSemester = element.semesterNo;
-//     }
-//   }
-// }
-
-// const semester = ref(0);
-
-// function smsChoosen(id) {
-//   semester.value = id;
-//   console.log(semester.value);
-// }
-
 
 </script>
 <template>
-<h1></h1>
-    <div id="task">
-        <ul v-for="allData in allDatas" :key="allData.id" id="task2">
-
-            <div id="titre">{{ allData.name }}</div>
-            <div>{{ allData.description }}</div>
-            <!-- <div>{{ allData.dateStart }}</div>
-            <div>{{ allData.dateEnd }}</div> -->
-        </ul>
+    <div
+      v-for="allData in allDatas"
+      :key="allData.id"
+      class="course-ctn"
+    >
+      <div class="border"></div>
+      <div class="DailyCourseBox">
+        <p class="p bold task-name">
+          {{ allData.name }}
+        </p>
+        <p class="p task-description">
+          {{ allData.description }}
+        </p>
+      </div>
     </div>
-
-
-    <!-- <div v-show="student">
-        <h1>Saisir une review :</h1>
-        <select @change="test()">
-            <option>Sélectionnez un cours</option>
-            <option  v-for="reviewPossible in reviewsPossibles" :key="reviewPossible" :class="reviewPossible">
-                {{ reviewPossible }} </option>
-        </select>
-    </div> -->
+          <!-- <div>{{ allData.dateStart }}</div>
+            <div>{{ allData.dateEnd }}</div> -->
 </template>
 
 
-
-
-<!-- <template>
-  <h1>Notes</h1>
-  <div id="semester">
-    <div v-for="i in maxSemester" :key="i">
-      <base-button @click="smsChoosen(i)" type="submit">S{{ i }}</base-button>
-    </div>
-  </div>
-  <div id="body">
-    <base-box id="file">
-      <div v-for="result in allData" :key="result.grade">
-        <div>
-          <p>{{ result.moduleName }}</p>
-          <p>
-            <strong>{{ result.CourseName }}</strong> (coeff: {{ result.courseWeight }})
-          </p>
-          <p>{{ result.gradeType }} ({{ result.gradeWeight }}%) : <strong>{{ result.grade }}</strong></p>
-        </div>
-        <hr />
-      </div>
-    </base-box>
-
-    <base-button>Télécharger le pdf</base-button>
-  </div>
-</template> -->
-
 <style scoped>
-#body {
+/* #body {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-}
+} */
 
-h1 {
-  text-align: center;
-  margin-top: 1%;
-  margin-bottom: 1%;
-}
 
-#task {
+
+div {
   display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin-bottom: 1%;
-  background-color: var(--beige);
 }
 
-#task2 {
-  /* display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin-bottom: 1%; */
-  background-color: var(--beige);
-}
-
-#file {
-  display: flex;
+.DailyCourseBox {
   flex-direction: column;
-  padding: 1%;
-  width: 80%;
+  padding: 1em;
+  margin: 1rem 1rem 1rem 0rem;
+  background-color: #F1DFCD79;
+  border-radius: 0rem 0.3rem 0.3rem 0rem;
 }
 
-#titre {
-font-weight: bold;
+.border {
+  width: 1rem;
+  margin: 1rem 0rem 1rem 1rem;
+  background-color: #F1DFCD;
+  border-radius: 0.3rem 0rem 0rem 0.3rem;
 }
 </style>
