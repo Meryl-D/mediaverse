1f47501afc60aba4858ed2c81da05d86c2a25120
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2022-06-15 13:08:11 +0200

    created back button

---
+++ resources/js/components/subComponents/BaseBackButton.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { selectedDate, isActive } from "../../stores.js";
+
+
+const props = defineProps({
+  lessonDay: {
+    type: Object,
+    required: true,
+  },
+});
+
+function goBackWeeklyView() {
+  selectedDate.value = props.lessonDay;
+  isActive.value.weekly = true;
+  isActive.value.daily = false;
+  isActive.value.monthly = false;
+}
+</script>
+
+<template>
+  <span class="go-back"
+    ><button @click="goBackWeeklyView()">Horaire et tâches</button></span
+  >
+</template>
+
+<style scoped>
+button {
+  color: var(--green);
+  background-color: var(--white);
+  border: none;
+  border-radius: var(--small-radius);
+  box-shadow: var(--shadow);
+  padding: 0.3rem 0.6rem;
+}
+
+button:hover,
+button:active {
+  color: var(--orange);
+}
+</style>
